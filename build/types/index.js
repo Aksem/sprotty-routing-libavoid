797dde5a15af52f980f4336725e20547ccbc35bb
Vladyslav Hnatiuk <aders1234@gmail.com>
2021-06-09 09:51:22 +0200

    Enable all libs in build.

--- build/types/index.js
+++ build/types/index.js
@@ -48,12 +48,12 @@ export function load() {
     });
 }
 export * from 'sprotty/src/features/routing/anchor';
-// export * from "./routing/linear-edge-router";
-// export * from "./routing/manhattan-anchors";
-// export * from "./routing/manhattan-edge-router";
-// export * from "./routing/model";
-// export * from "./routing/polyline-anchors";
-// export * from "./routing/polyline-edge-router";
-// export * from "./routing/routing";
 export * from 'sprotty/src/features/routing/views';
+export * from "./routing/linear-edge-router";
+export * from "./routing/manhattan-anchors";
+export * from "./routing/manhattan-edge-router";
+export * from "./routing/model";
+export * from "./routing/polyline-anchors";
+export * from "./routing/polyline-edge-router";
+export * from "./routing/routing";
 //# sourceMappingURL=index.js.map
--- build/types/sprotty-routing-libavoid/src/lib/index.d.ts
+++ build/types/sprotty-routing-libavoid/src/lib/index.d.ts
@@ -1,4 +1,11 @@
 export declare function load(): Promise<void>;
 export * from 'sprotty/src/features/routing/anchor';
 export * from 'sprotty/src/features/routing/views';
+export * from "./routing/linear-edge-router";
+export * from "./routing/manhattan-anchors";
+export * from "./routing/manhattan-edge-router";
+export * from "./routing/model";
+export * from "./routing/polyline-anchors";
+export * from "./routing/polyline-edge-router";
+export * from "./routing/routing";
 //# sourceMappingURL=index.d.ts.map
