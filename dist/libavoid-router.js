62e55670c3e59cd786113adc44f5e4ae562917ac
Vladyslav Hnatiuk <aders1234@gmail.com>
2024-05-13 16:27:59 +0200

    v1.2.1: clean debug logs

--- dist/libavoid-router.js
+++ dist/libavoid-router.js
@@ -1,7 +1,5 @@
 import { AvoidLib } from "libavoid-js";
-import { SRoutingHandleImpl, EdgeRouting, AbstractEdgeRouter, isBoundsAware, SConnectableElementImpl, 
-// isConnectable,
-SParentElementImpl, connectableFeature, SShapeElementImpl } from "sprotty";
+import { SRoutingHandleImpl, EdgeRouting, AbstractEdgeRouter, isBoundsAware, SConnectableElementImpl, SParentElementImpl, connectableFeature } from "sprotty";
 import { Point, centerOfLine } from "sprotty-protocol";
 import { RouteType, Directions, libavoidRouterKind, } from "./libavoid-router-options";
 import { LibavoidEdge } from "./libavoid-edge";
@@ -173,17 +171,14 @@ export class LibavoidRouter extends AbstractEdgeRouter {
             };
         }
         sprottyRoute.push(Object.assign({ kind: "target" }, targetAnchor));
-        console.log('route', edge.id, sprottyRoute);
         this.edgeRouting.set(edge.id, sprottyRoute);
     }
     routeAll(edges, parent) {
-        console.log('route all');
         const Avoid = AvoidLib.getInstance();
         let routesChanged = false;
         // add shapes to libavoid router
         const connectables = this.getAllBoundsAwareChildren(parent);
         for (const child of connectables) {
-            console.log(child, child instanceof SConnectableElementImpl, child instanceof SShapeElementImpl);
             if (!(child instanceof SConnectableElementImpl) || !child.hasFeature(connectableFeature)) {
                 continue;
             }
@@ -319,7 +314,6 @@ export class LibavoidRouter extends AbstractEdgeRouter {
     }
     route(edge, args) {
         var _a, _b, _c, _d;
-        console.log('route single', edge);
         let route = this.edgeRouting.get(edge.id);
         if (route === undefined) {
             // edge cannot be routed yet(e.g. pre-rendering phase), but glsp server requires at least
